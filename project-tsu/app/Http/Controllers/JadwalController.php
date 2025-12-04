b019728dfd28899b3293897f2fc324e79c540294
gumelar11 <adagumelar17@gmail.com>
2025-12-04 19:15:51 +0700

    update fitur penjadwalan step 1-4

--- project-tsu/app/Http/Controllers/JadwalController.php
+++ project-tsu/app/Http/Controllers/JadwalController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Jadwal;
+use App\Models\Kurikulum;
+use App\Models\Ruangan;
+use App\Models\Dosen;
+use App\Models\Waktu;
+use App\Models\Hari;
+use App\Models\MataKuliah;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facade\Excel;
 use App\Export\JadwalExport;
@@ -10,51 +16,140 @@ use PDF;
 
 class JadwalController extends Controller
 {
-    //read dan filter
-    public function index(Request $request)
-    {
-        $semester = $request->get('semester');
-
-        $jadwal = Jadwal::query();
-
-        if ($semester) {
-            $jadwal->whereHas('matkul', function ($q) use ($semester){
-                $q->where('semester', $semester);
-            });
-        }
-        $jadwal = $jadwal->with(['matkul', 'dosen', 'kelas', 'ruang', 'hari', 'slot'])->get();
-        return view();
+    
+    public function index(){
+        $step = session('penjadwalan.current_step', 1);
+        
+        return view('penjadwalan.penjadwalan', [
+            'step' => $step,
+            'kurikulums' => Kurikulum::all(),
+            'ruangans' => Ruangan::all(),
+            'dosens' => Dosen::all(),
+            'waktus' => Waktu::all(),
+            'haris' => Hari::all(),
+            'matkuls' => MataKuliah::all(),
+        ]);
     }
 
-    //tambah - form
-    public function store(Request $request)
+    public function proses(Request $r)
     {
-        $request->validate([
-            'id_matkul' => 'required',
-            'id_dosen' => 'required',
-            'id_kelas' => 'required',
-            'id_ruang'  => 'required',
-            'id_hari' => 'required',
-            'id_slot' => 'required',
-            'jenis_jadwal' => 'required',
+        $step = $r->step;
+
+        if ($step == 1) return $this->handleStep1($request);
+        if ($step == 2) return $this->handleStep2($request);
+        if ($step == 3) return $this->hanldeStep3($request);
+        if ($step == 4) return $this->hanldeStep4();
+    }
+
+    public function handleStep1(Request $r)
+    {
+        $r->validated([
+            'id_kurikulum' => 'required',
+            'semester' => 'required|array|min:1',
         ]);
 
-        //bentrokan
-        $bentrok = Jadwal::where('id_hari', $request->id_hari)
-        ->where('id_slot', $request->id_slot)
-        ->where(function ($q) use ($request) {
-            $q->where('id_dosen', $request->id_dosen)
-            ->orWhere('id_ruang', $request->id_ruang)
-            ->orWhere('id_kelas', $request->id_kelas);
-        })
-        ->exist();
-
-        if ($bentrok) {
-            return redirect()->back()->with('error','Jadwal bentrok! Dosen, ruang, atau kelas sudah terpakai.');
-        }
+        session([
+            'penjadwalan.id_kurikulum' => $r->id_kurikulum,
+            'penjadwalan.semester' => $r->semester,
+            'penjadwalan.current_step' => 2,
+        ]);
         
-        Jadwal::create($request->all());
-        return redirect()->route();
+        return back();
+    }
+
+    public function handleStep2(Request $r)
+    {
+        $r->validate([
+            'id_ruang' => 'required|array|min:1',
+        ]);
+
+        session([
+            'penjadwalan.id_ruang' => $r->id_ruang,
+            'penjadwalan.current_step' => 3,
+        ]);
+
+        return back();
+    }
+
+    public function handleStep3(Request $r)
+    {
+        $kurikulum = session('penjadwalan.id_kurikulum');
+        $semester = session('penjadwalan.semester');
+
+        //QUERY
+        $matkul = MataKuliah::where(id_kurikulum, $kurikulum)
+                    ->whereIn('semester', $semester)
+                    ->get();
+        
+        session([
+            'penjadwalan.matkul_list' => $matkul,
+            'penjadwalan.current_step'=> 4,
+        ]);
+
+        return back();
+    }
+
+    public function handleStep4()
+    {
+        $matkuls = session('penjadwalan.matkul_list', []);
+        $ruang = session('penjadwalan.id_ruang', []);
+
+        if (empty($matkuls)) {
+            return back()->with('error', "Tidak ada matakuliah yang dipilih");
+        }
+
+        $dataMK = MataKuliah::whereIn('id_matkul', $matkuls)
+                    ->orderBy('sks', 'DESC')
+                    ->get();
+        $hari = Hari::all();
+        $waktu = Waktu::all();
+        $ruangan = Ruangan::whereIn('id_ruang', $ruang)->get();
+
+        $gagal = [];
+
+        foreach ($dataMK as $mk) {
+            $dosen = $mk->id_dosen;
+            $kelas = $mk->id_kelas;
+
+            $sukses = false;
+
+            foreach ($hari as $h) {
+                foreach ($waktu as $w) {
+                    $bentrok = Jadwal::where('id_hari', $h->id_hari)
+                        ->where('id_slot', $w->id_slot)
+                        ->where(function ($q) use ($dosen, $kelas) {
+                            $q->where('id_dosen', $dosen)
+                            ->orWhere('id_kelas', $kelas);
+                        })->exists();
+
+                    if ($bentrok) continue;
+                    $ruangKosong = Ruangan::whereIn('id_ruang', $ruang)
+                        ->whereDoesntHave('jadwal', function ($q) use ($h, $w) {
+                            $q->where('id_hari', $h->id_hari)
+                            ->where('id_slot', $w->id_slot);
+                        })->first();
+                    
+                    if (!$ruangKosong) continue;
+
+                    Jadwal::create([
+                        'id_matkul' => $mk->id_matkul,
+                        'id_dosen' => $dosen,
+                        'id_kelas' => $kelas,
+                        'id_ruang' => $ruangKosong->id_ruang,
+                        'id_hari' => $h->id_hari,
+                        'id_slot' => $w->id_slot,
+                        'jenis_jadwal' => $mk->jenis,
+                        'status_validasi' => 0,
+                    ]);
+
+                    $sukses = true;
+                    break;
+                }
+                if ($sukses) break;
+            }
+            if (!$sukses) $gagal[] = $mk->nama_matkul;
+        }
+        return back()-with('gagal', $gagal);
     }
     
     //EXCEL
